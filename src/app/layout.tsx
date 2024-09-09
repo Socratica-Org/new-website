82cd4cc8ceee4d404715d59de37229612e5d3fe7
Eesha <eeshaulh@gmail.com>
2024-09-09 15:05:08 -0400

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Footer from './components/footer'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <link rel="icon" href="/favicon.ico" sizes="any" />
         {children}
+        <Footer/>
       </body>
     </html>
   );
